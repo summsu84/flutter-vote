11094b24a55e951f875657921eb6e6a437891659
summsu84 <summsu84@gmail.com>
2019-09-06 09:50:44 +0900

    댓글 화면 업데이트

--- lib/screen/comment/index.dart
+++ lib/screen/comment/index.dart
@@ -6,10 +6,12 @@ import "dart:async";
 
 import 'package:flutter_vote/repository/repository.dart';
 
-// 댓글 화면
+/// 댓글 화면
+/// 1. 추가작업 내용
+/// - 페이징 기법
+/// - 댓글 등록 후 리프레쉬 기능
 class CommentScreen extends StatefulWidget {
   final String voteId;
-
   const CommentScreen({this.voteId});
 
   // 갱신 시 _CommentScreenState 호출
@@ -27,38 +29,178 @@ class _CommentScreenState extends State<CommentScreen> {
 
   _CommentScreenState({this.voteId});
 
-  @override
-  void initState() {
-    super.initState();
-    fetchComment();
+  //FutureBuilder로 가져오기
+  // 리스트 뷰
+  buildListView(BuildContext context, AsyncSnapshot snapshot) {
+    List<DocumentSnapshot> values = snapshot.data;
+    if (values != null) {
+      return Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              itemCount: values.length,
+              padding: EdgeInsets.all(5.0),
+              itemBuilder: (context, i) {
+                return buildListTile2(Comment.fromDocument(values[i]));
+              },
+            ),
+          ),
+          Divider(),
+        ],
+      );
+    } else {
+      return Text('Loading, Please wait..');
+    }
   }
 
-  // Comment 가져오기
-  void fetchComment() async {
-    //FirebaseUser currentUser = await _repository.getCurrentUser();
-
-    List<DocumentSnapshot> documentSnapshot =
-        await _repository.fetchComment(voteId);
-    setState(() {
-      this.documentSnapshot = documentSnapshot;
-      for (var doc in this.documentSnapshot) {
-        comments.add(Comment.fromDocument(doc));
-      }
-    });
-    /*User user = await _repository.fetchUserDetailsById(currentUser.uid);
-    setState(() {
-      this.currentUser = user;
-    });*/
-
-    //_future =  _repository.fetchComment(voteId);
-
-/*    User user = await _repository.fetchUserDetailsById(currentUser.uid);
-    setState(() {
-      this.currentUser = user;
-    });*/
+  buildListTile2(Comment item) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: SizedBox(
+        height: 120,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            CircleAvatar(
+              backgroundImage: NetworkImage(item.avatar),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20.0, 0.0, 2.0, 0.0),
+                child: buildDescription(item),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 
-  @override
+  buildDescription(Comment item) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+          flex: 2,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                item.userId,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Padding(padding: EdgeInsets.only(bottom: 2.0)),
+              Text(
+                item.comment,
+                maxLines: 4,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontSize: 14.0,
+                  color: Colors.black54,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text('2일전 ★',
+                          style: const TextStyle(
+                            fontSize: 12.0,
+                            color: Colors.black54,
+                          )),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      new Icon(Icons.thumb_up, color: Colors.grey, size: 18),
+                      Text('100',
+                          style: const TextStyle(
+                            fontSize: 12.0,
+                            color: Colors.black54,
+                          )),
+                      new SizedBox(
+                        width: 16.0,
+                      ),
+                      new Icon(Icons.thumb_down, color: Colors.grey, size: 18),
+                      Text('100',
+                          style: const TextStyle(
+                            fontSize: 12.0,
+                            color: Colors.black54,
+                          )),
+                      new SizedBox(
+                        width: 16.0,
+                      ),
+                    ],
+                  )
+                ],
+              )
+              /*Text(
+                '2일전 ★',
+                style: const TextStyle(
+                  fontSize: 12.0,
+                  color: Colors.black54,
+                ),
+              ),*/
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget build(BuildContext context) {
+    var futureBuilder = new FutureBuilder(
+      future: _getVoteComments(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+          case ConnectionState.waiting:
+            return new Text('댓글 가져오는 중...');
+          default:
+            if (snapshot.hasError)
+              return new Text('Error: ${snapshot.error}');
+            else
+              return buildPage(context, snapshot);
+        }
+      },
+    );
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "댓글보기",
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: futureBuilder,
+    );
+  }
+
+  ///투표에 대한 댓글 정보를 가져온다.
+  /// Future async -> Future async 로 가져가야 할지?? 테스트 필요
+  Future<List<DocumentSnapshot>> _getVoteComments() async {
+    //콜백 처리
+    var voteDocuments = await _repository.fetchComment(this.voteId);
+    print(voteDocuments.length);
+    return voteDocuments;
+  }
+
+/*  @override
   Widget build(BuildContext context) {
     // Scaffold Layout
     return Scaffold(
@@ -71,17 +213,17 @@ class _CommentScreenState extends State<CommentScreen> {
       ),
       body: buildPage(),
     );
-  }
+  }*/
 
   // 위젯 생성
-  Widget buildPage() {
+  Widget buildPage(BuildContext context, AsyncSnapshot snapshot) {
     return Column(
       children: [
         // 화면 채우기
         Expanded(
           child:
               //buildComments(),
-              buildListView(),
+              buildListView(context, snapshot),
         ),
         Divider(),
         // 아래는 댓글 쓰기 위젯
@@ -103,34 +245,14 @@ class _CommentScreenState extends State<CommentScreen> {
     );
   }
 
-  buildListView() {
-    if (comments != null) {
-      return Column(
-        children: <Widget>[
-          Expanded(
-            child: ListView.builder(
-              itemCount: comments.length,
-              padding: EdgeInsets.all(5.0),
-              itemBuilder: (context, i) {
-                //return buildListTile(comments.elementAt(i));
-                return commentItem(comments.elementAt(i));
-              },
-            ),
-          ),
-          Divider(),
-        ],
-      );
-    } else {
-      return Text('Loading, Please wait..');
-    }
-  }
+
 
   buildListTile(Comment item) {
     return Column(
       children: <Widget>[
         ListTile(
           title: Column(
-            children: <Widget>[Text("테스트"),Text(item.comment)],
+            children: <Widget>[Text("테스트"), Text(item.comment)],
           ),
           leading: CircleAvatar(
             backgroundImage: NetworkImage(item.avatar),
@@ -152,7 +274,6 @@ class _CommentScreenState extends State<CommentScreen> {
     //    time =timestamp.toDate().toString();
     //    dateAndTime = time.split(" ");
     //   }
-
 
     return Padding(
       padding: const EdgeInsets.all(12.0),
@@ -249,34 +370,7 @@ class _CommentScreenState extends State<CommentScreen> {
       uid: 'uid',
     );
     _repository.addComment(currentUser, voteId, comment);
-    /*
-    Firestore.instance
-        .collection("vote_comments")
-        .document(postId)
-        .collection("comments")
-        .add({
-      "username": 'tester',
-      "comment": comment,
-      "timestamp": DateTime.now().toString(),
-      "avatarUrl": 'https://www.caralyns.com/wp-content/uploads/2014/10/sample-avatar.png',
-      "userId": 'tester'
-    });
 
-    //adds to postOwner's activity feed
-    Firestore.instance
-        .collection("insta_a_feed")
-        .document(postOwner)
-        .collection("items")
-        .add({
-      "username": 'tester',
-      "userId": 'tester',
-      "type": "comment",
-      "userProfileImg": 'https://www.caralyns.com/wp-content/uploads/2014/10/sample-avatar.png',
-      "commentData": comment,
-      "timestamp": DateTime.now().toString(),
-      "postId": postId,
-      "mediaUrl": postMediaUrl,
-    });*/
   }
 }
 
